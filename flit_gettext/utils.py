5855b514c03440136982213e6fef0ddb5338ac01
Johannes Maron <johannes@maron.family>
2023-10-20 16:38:49 +0200

    Add humand readable error message for missing gettext dependency

--- flit_gettext/utils.py
+++ flit_gettext/utils.py
@@ -1,3 +1,4 @@
+import shutil
 import subprocess  # nosec
 from pathlib import Path
 
@@ -5,8 +6,14 @@ from pathlib import Path
 def compile_gettext_translations(config):
     """Compile gettext translations."""
     print("\33[1m* Compiling gettext translations...\33[0m")
+
+    msgfmt = shutil.which("msgfmt")
+
+    if msgfmt is None:
+        raise OSError("msgfmt not found, please install gettext or check your PATH.")
+
     # working directory is the source tree
     for file in Path().rglob(f"{config.module}/**/*.po"):
         file = Path(file)
-        cmd = ["msgfmt", "-c", "-o", file.parent / f"{file.stem}.mo", file]
+        cmd = [msgfmt, "-c", "-o", file.parent / f"{file.stem}.mo", file]
         subprocess.check_output(cmd)  # nosec
